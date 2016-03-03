47028193c7943d59c15dc47df60d89c707966c79
orangepek0e <georgejohnston13@hotmail.com>
2016-03-02 20:28:50 -0400

    Video upload works in theory

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.User'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.User', 'starter.Video'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', ['ngStorage'])
+angular.module('starter.controllers', ['ngStorage', 'ngCordova'])
 
-  .controller('DashCtrl', function ($scope, $ionicLoading, $ionicModal, $localStorage, $http) {
+  .controller('DashCtrl', function ($scope, $ionicLoading, $ionicModal, $localStorage, $http, $cordovaCapture, VideoService) {
     $scope.posts = [];
     $scope.newPost = {};
     $scope.newPost.wifi = false;
@@ -132,6 +132,30 @@ angular.module('starter.controllers', ['ngStorage'])
 
       }
 
+    }
+
+    $scope.clip = '';
+
+    $scope.captureVideo = function() {
+      $cordovaCapture.captureVideo().then(function(videoData) {
+        VideoService.saveVideo(videoData).success(function(data) {
+          $scope.clip = data;
+          $scope.$apply();
+        }).error(function(data) {
+          console.log('ERROR: ' + data);
+        });
+      });
+    };
+
+    $scope.urlForClipThumb = function(clipUrl) {
+      var name = clipUrl.substr(clipUrl.lastIndexOf('/') + 1);
+      var trueOrigin = cordova.file.dataDirectory + name;
+      var sliced = trueOrigin.slice(0, -4);
+      return sliced + '.png';
+    }
+
+    $scope.showClip = function(clip) {
+      console.log('show clip: ' + clip);
     }
   })
 
